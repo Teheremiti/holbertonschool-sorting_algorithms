282202e1a1ed8bd8f89b75880ed63557e87cbc6d
truuue <noahvernhet@gmail.com>
2023-05-23 06:03:09 -0700

    Correct code for task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,29 +6,29 @@
  */
 void selection_sort(int *array, size_t size)
 {
-        size_t i = 0;
-        size_t j = 0;
-        int runner = 0;
-        int tmp = 0;
+	size_t i = 0;
+	size_t j = 0;
+	int runner = 0;
+	int tmp = 0;
 
-        if (!array || !size)
-            return;
-        while (i <= size - 1)
-        {
-                while (j <= size - 1)
-                {
-                        runner = array[i];
-                        j++;
-                        if (array[j] < runner)
-                        {
-                                tmp = array[j];
-                                array[j] = runner;
-                                runner = tmp;
-                                array[i] = runner;
-                                print_array(array, size);
-                        }
-                }
-                i++;
-                j = i;
-        }
+	if (!array || !size)
+		return;
+	while (i < size - 1)
+	{
+		while (j < size - 1)
+		{
+			runner = array[i];
+			j++;
+			if (array[j] < runner)
+			{
+				tmp = array[j];
+				array[j] = runner;
+				runner = tmp;
+				array[i] = runner;
+				print_array(array, size);
+			}
+		}
+		i++;
+		j = i;
+	}
 }
