75b83f46f1355ce478b52a15ae56fa923f444b33
truuue <noahvernhet@gmail.com>
2023-05-23 05:58:14 -0700

    Add task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array in ascending order with selection sort
+ * @array: array to be sorted
+ * @size: max size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+        size_t i = 0;
+        size_t j = 0;
+        int runner = 0;
+        int tmp = 0;
+
+        if (!array || !size)
+            return;
+        while (i <= size - 1)
+        {
+                while (j <= size - 1)
+                {
+                        runner = array[i];
+                        j++;
+                        if (array[j] < runner)
+                        {
+                                tmp = array[j];
+                                array[j] = runner;
+                                runner = tmp;
+                                array[i] = runner;
+                                print_array(array, size);
+                        }
+                }
+                i++;
+                j = i;
+        }
+}
